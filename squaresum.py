81482011c2fd5e9fc97305e1d34be84be84f1a6c
poseidon2022 <114685152+poseidon2022@users.noreply.github.com>
2023-07-03 00:50:51 +0300

    Add files via upload

---
+++ squaresum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        i = 0
+        j = int(c**(1/2))
+        while i<=j:
+            if i*i + j*j < c:
+                i+=1
+            elif i*i + j*j > c:
+                j-=1
+            else:
+                return True
+        return False
