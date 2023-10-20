06ba1a9aa9a53772bcc04e6fd1b01223b01514eb
poseidon2022 <114685152+poseidon2022@users.noreply.github.com>
2023-10-20 14:07:42 +0300

    Add files via upload

--- range_sum.py
+++ range_sum.py
@@ -1,9 +1,16 @@
-class NumArray:
+class NumMatrix:
 
-    def __init__(self, nums: List[int]):
-        self.nums = nums
-    def sumRange(self, left: int, right: int) -> int:
-        prefix = self.nums[:]
-        for i in range(left+1,right+1):
-            prefix[i] = prefix[i-1] + self.nums[i]
-        return prefix[right]
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+        
+        for i in range(len(matrix)):
+            for j in range(1,len(matrix[i])):
+                matrix[i][j] = matrix[i][j-1] + matrix[i][j]
+        print(matrix)
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        total = 0
+        for i in range(row1,row2+1):
+            if col1-1>=0:
+                total+= self.matrix[i][col2]-self.matrix[i][col1-1]
+            else: total+=self.matrix[i][col2]
+        return total
